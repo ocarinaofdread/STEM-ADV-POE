3bb5b2b002144b20876411f91aa64e9e357d645a
dreaded <renchase93@gmail.com>
2025-04-27 00:21:46 -0400

    FINISHED SPELL SWAPPING
    YEAAAAAAAAAAAAAAAAH

--- Assets/_1_Our Assets/Scripts/Spell System/SpellHandler.cs
+++ Assets/_1_Our Assets/Scripts/Spell System/SpellHandler.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,8 +11,7 @@ public class SpellHandler : MonoBehaviour
     [SerializeField] private InputActionReference[] exemptedActionList;
 
     [SerializeField] private InputActionReference navigationEnableAction;
-    [SerializeField] private InputActionReference positiveNavigationAction;
-    [SerializeField] private InputActionReference negativeNavigationAction;
+    [SerializeField] private InputActionReference navigationInputAction;
 
     private GameManager _gameManager;
     private GameObject[] _spellPrefabList;
@@ -21,6 +19,7 @@ public class SpellHandler : MonoBehaviour
     private int _currentSpellPrefabIndex;
     private int _exemptedButtonsPressed;
     private bool _canSwitchSpells;
+    private bool _isHoldingGrimoire;
 
     void Start()
     {
@@ -33,22 +32,26 @@ public class SpellHandler : MonoBehaviour
     // Input References
     void OnEnable()
     {
-        foreach (var thisCastAction in castActionList) {
-            thisCastAction.action.performed += OnCastAction; }
+        foreach (var thisCastAction in castActionList)
+        {
+            thisCastAction.action.performed += OnCastAction;
+        }
 
-        foreach (var thisExemptedAction in exemptedActionList) {
+        foreach (var thisExemptedAction in exemptedActionList)
+        {
             thisExemptedAction.action.performed += AddExemptions;
-            thisExemptedAction.action.canceled += SubtractExemptions; }
+            thisExemptedAction.action.canceled += SubtractExemptions;
+        }
 
         // As long as navigationEnableAction is selected, spells can be cast
         navigationEnableAction.action.performed += EnableGrimoireNavigation;
         navigationEnableAction.action.canceled += DisableGrimoireNavigation;
-        
-        positiveNavigationAction.action.performed += sender => OnNavigationAction(sender, 1);
-        negativeNavigationAction.action.performed += sender => OnNavigationAction(sender, -1);
+
+        navigationInputAction.action.performed += OnNavigationAction;
+
     }
 
-    
+
 
     void OnCastAction(InputAction.CallbackContext obj)
     {
@@ -58,28 +61,53 @@ public class SpellHandler : MonoBehaviour
         }
     }
 
-    void OnNavigationAction(InputAction.CallbackContext obj, int incrementValue)
+    void OnNavigationAction(InputAction.CallbackContext obj)
     {
-        if (_canSwitchSpells)
+        if (!_canSwitchSpells || !_isHoldingGrimoire)
         {
-            _currentSpellPrefabIndex += incrementValue;
-            if (_currentSpellPrefabIndex >= _spellPrefabList.Length) {
-                _currentSpellPrefabIndex = 0; }
-            else if (_currentSpellPrefabIndex < 0) {
-                _currentSpellPrefabIndex = _spellPrefabList.Length - 1; }
-
-            _currentSpellPrefab = _spellPrefabList[_currentSpellPrefabIndex];
-            grimoire.UpdateGrimoirePages(_currentSpellPrefab.GetComponent<Spell>().GetName(), 1);
+            return;
         }
-    }
-    
-    void EnableGrimoireNavigation(InputAction.CallbackContext obj) { _canSwitchSpells = true; }
-    void DisableGrimoireNavigation(InputAction.CallbackContext obj) { _canSwitchSpells = false; }
 
-    void AddExemptions(InputAction.CallbackContext obj) { _exemptedButtonsPressed += 1;}
-    void SubtractExemptions(InputAction.CallbackContext obj) { _exemptedButtonsPressed -= 1;}
-    
+        var incrementValue = (int)Math.Round(obj.ReadValue<Vector2>().x);
+
+        _currentSpellPrefabIndex += incrementValue;
+        if (_currentSpellPrefabIndex >= _spellPrefabList.Length)
+        {
+            _currentSpellPrefabIndex = 0;
+        }
+        else if (_currentSpellPrefabIndex < 0)
+        {
+            _currentSpellPrefabIndex = _spellPrefabList.Length - 1;
+        }
+
+        _currentSpellPrefab = _spellPrefabList[_currentSpellPrefabIndex];
+        grimoire.UpdateGrimoirePages(_currentSpellPrefab.GetComponent<Spell>().GetName(), 1);
+    }
+
+    void EnableGrimoireNavigation(InputAction.CallbackContext obj)
+    {
+        _canSwitchSpells = true;
+    }
+
+    void DisableGrimoireNavigation(InputAction.CallbackContext obj)
+    {
+        _canSwitchSpells = false;
+    }
+
+    void AddExemptions(InputAction.CallbackContext obj)
+    {
+        _exemptedButtonsPressed += 1;
+    }
+
+    void SubtractExemptions(InputAction.CallbackContext obj)
+    {
+        _exemptedButtonsPressed -= 1;
+    }
+
+    public void StartHoldingGrimoire() { _isHoldingGrimoire = true; }
+    public void StopHoldingGrimoire() { _isHoldingGrimoire = false; }
+
     // Get/Set Functions
-    public void SetCurrentSpell(GameObject spell) { _currentSpellPrefab = spell; }
-    public GameObject GetCurrentSpell() { return _currentSpellPrefab; }
+    //public void SetCurrentSpell(GameObject spell) { _currentSpellPrefab = spell; }
+    //public GameObject GetCurrentSpell() { return _currentSpellPrefab; }
 }
