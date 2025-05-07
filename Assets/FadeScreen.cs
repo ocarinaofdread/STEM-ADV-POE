81406c391bc483b6f0af918f044a66953565f936
Chaotic-time <aszeles25@gmail.com>
2025-05-07 15:49:43 -0400

    hell on earth

--- Assets/FadeScreen.cs
+++ Assets/FadeScreen.cs
@@ -11,10 +11,32 @@ public class FadeScreen : MonoBehaviour
     {
         rend = GetComponent<Renderer>();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void FadeIn()
     {
-        
+        Fade(1, 0);
+    }
+    public void FadeOut()
+    {
+        Fade(0, 1);
+    }
+    public void Fade(float alphaIn, float alphaOut)
+    {
+        StartCoroutine(FadeRoutine(alphaIn, alphaOut));
+    }
+
+    public IEnumerator FadeRoutine(float alphaIn, float alphaOut)
+    {
+        float timer = 0;
+        while (timer <= fadeDuration)
+        {
+            Color newColor= fadeColor;
+            newColor.a = Mathf.Lerp(alphaIn, alphaOut, timer/fadeDuration);
+            rend.material.SetColor("color", newColor);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        Color newColor2 = fadeColor;
+        newColor2.a = alphaOut;
+        rend.material.SetColor("color", newColor2);
     }
 }
---
+++ Assets/SceneManagerController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerController : MonoBehaviour
+{
+    
+    public void LoadScene(int sceneInt)
+    {
+        SceneManager.LoadScene(sceneInt);
+    }
+}
