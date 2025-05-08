bd256d9f4ddf78fa9b63e47696a2af0aed031b5d
dreaded <146963301+ocarinaofdread@users.noreply.github.com>
2025-05-07 21:31:10 -0400

    Began projectile launching
    Scrapped projectile motion via Ms. Snyder's formula due to requiring the use of quartic functions, found script by aldonaletto on Unity Discussions

--- Assets/_1_Our Assets/Scripts/Utility/LaunchRockProjectile.cs
+++ Assets/_1_Our Assets/Scripts/Utility/LaunchRockProjectile.cs
@@ -5,8 +5,9 @@ using UnityEngine.XR.Content.Interaction;
 
 public class LaunchRockProjectile : MonoBehaviour
 {
-    [SerializeField] private float launchSpeed;
-    [SerializeField] private Vector3 launchDirection;
+    [SerializeField] private float launchAngle;
+    [SerializeField] private Transform targetTransform;
+    // [SerializeField] private Transform playerTransform;
     [SerializeField] private bool launchLocal;
 
     private Rigidbody _rigidbody;
@@ -20,17 +21,24 @@ public class LaunchRockProjectile : MonoBehaviour
 
     private void Launch()
     {
-        if (launchLocal)
-        {
-            //launchDirection = GetLocalDirection(launchDirection);
-        }
-
-        Debug.Log("Launch speed * Launch direction: " + (launchSpeed * launchDirection));
-        _rigidbody.AddForce(launchSpeed * launchDirection, ForceMode.Impulse);
+        _rigidbody.velocity = BallisticVel(targetTransform, launchAngle);
+    }
+    
+    
+    // Code taken from aldonaletto on Unity Discussions
+    
+    Vector3 BallisticVel(Transform target, float angle){
+        Vector3 dir = target.position - transform.position;  // get target direction
+        float h = dir.y;  // get height difference
+        
+        dir.y = 0;  // retain only the horizontal direction
+        float dist = dir.magnitude ;  // get horizontal distance
+        float a = angle * Mathf.Deg2Rad;  // convert angle to radians
+        dir.y = dist * Mathf.Tan(a);  // set dir to the elevation angle
+        dist += h / Mathf.Tan(a);  // correct for small height differences
+        // calculate the velocity magnitude
+        float vel = Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a));
+        return vel * dir.normalized;
     }
 
-    private Vector3 GetLocalDirection(Vector3 direction)
-    {
-        return transform.InverseTransformPoint(direction);
-    }
 }
