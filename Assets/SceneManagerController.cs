212aa2355e8c5174ed40b3b49d3490e478be0810
Chaotic-time <aszeles25@gmail.com>
2025-05-16 15:47:03 -0400

    LOTS OF STUFF

--- Assets/SceneManagerController.cs
+++ Assets/SceneManagerController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneManagerController : MonoBehaviour
 {
-    
+
     public void LoadScene(int sceneInt)
     {
         SceneManager.LoadScene(sceneInt);
