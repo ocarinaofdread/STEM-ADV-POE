76da70b167e8a4b914ee3ccc8edb0289fd9c4842
dreaded <renchase93@gmail.com>
2025-05-27 20:34:23 -0400

    Fixed Grimoire Interaction
    Now there's an OnSelectExiting section. No more dual casting! Or casting without the Grimoire. Do. Not. Let go of the Grimoire.

--- Assets/_1_Our Assets/Scripts/Systems/Spell System/GrimoireInteractor.cs
+++ Assets/_1_Our Assets/Scripts/Systems/Spell System/GrimoireInteractor.cs
@@ -34,4 +34,25 @@ public class GrimoireInteractor : XRGrabInteractable
 
         base.OnSelectEntering(args);
     }
+    
+    protected override void OnSelectExiting(SelectExitEventArgs args)
+    {   
+        // Remember: You hold the grimoire in your NON-DOMINANT HAND
+        if (args.interactorObject.transform.CompareTag("LeftHand"))
+        {
+            _rightHandSpellHandler ??= GameObject.FindGameObjectWithTag("RightHand").GetComponent<SpellHandler>();
+            //attachTransform = leftHandAttachTransform;
+            _rightHandSpellHandler.StopHoldingGrimoire();
+            //_gameManager.SetDominantHand(DominantHand.RightHanded);
+        }
+        else if (args.interactorObject.transform.CompareTag("RightHand"))
+        {
+            _leftHandSpellHandler ??= GameObject.FindGameObjectWithTag("LeftHand").GetComponent<SpellHandler>();
+            //attachTransform = rightHandAttachTransform;
+            _leftHandSpellHandler.StopHoldingGrimoire();
+            //_gameManager.SetDominantHand(DominantHand.LeftHanded);
+        }
+
+        base.OnSelectExiting(args);
+    }
 }
